f2e44e737338c6cf90c30a4a86ae931df0627f3c
Carina Abreu <carina.vas.abreu@gmail.com>
2022-09-27 23:00:11 -0300

    first

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,18 @@
+const FeedbackData = [
+  {
+    id: 1,
+    rating: 10,
+    text: "bla bla lba bla kdfjskdvmsjkdvk",
+  },
+  {
+    id: 2,
+    rating: 9,
+    text: "bla bla lba bla kdfjskdvmsjkdvk",
+  },
+  {
+    id: 3,
+    rating: 8,
+    text: "bla bla lba bla kdfjskdvmsjkdvk",
+  },
+];
+export default FeedbackData;
